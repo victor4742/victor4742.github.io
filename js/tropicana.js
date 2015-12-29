4620542c4d70860f1e0bde2d270628078ced5e9f
victor4742 <romero4742@hotmail.com>
2015-12-28 21:49:11 -0800

    First commit

---
+++ js/tropicana.js
@@ -0,0 +1,35 @@
+
+/*side bar navigation set items as active when clicked*/
+function navActive(item){
+    var url = item.href;
+    $('.my-side-nav .nav').find('.active').removeClass('active');
+    $('.my-side-nav .nav li a').each(function () {
+    	if (this.href == url) {
+        	$(this).parent().addClass('active');
+    	}
+    }); 
+};
+
+
+/** update sidebar */
+var lion = $('#lion').offset().top;
+var calFlame = $('#cal-flame').offset().top;
+var fireMagic = $('#fire-magic').offset().top;
+
+
+$(window).scroll(function(){
+
+	//get container scroll position
+	var fromTop = $(this).scrollTop();
+
+	if(fromTop >= fireMagic){
+		$('.my-side-nav .nav li').removeClass('active');
+		$('.side-nav3').addClass('active')
+	} else if(fromTop >= calFlame) {
+		$('.my-side-nav .nav li').removeClass('active');
+		$('.side-nav2').addClass('active');
+	} else {
+		$('.my-side-nav .nav li').removeClass('active');
+		$('.side-nav1').addClass('active');
+	}
+});
