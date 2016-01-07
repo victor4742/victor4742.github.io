a65d57d6c76795d1059249da695275961aa53c26
victor4742 <romero4742@hotmail.com>
2016-01-06 22:39:09 -0800

    updated contact layout, fixed take me to tropicana button, changed font color, changed sidebar width for grills.html

--- js/tropicana.js
+++ js/tropicana.js
@@ -1,24 +1,24 @@
 
 /*side bar navigation set items as active when clicked*/
-function navActive(item){
+/*function navActive(item){
     var url = item.href;
     $('.my-side-nav .nav').find('.active').removeClass('active');
     $('.my-side-nav .nav li a').each(function () {
     	if (this.href == url) {
+    		console.log(this.href + "  " + url);
         	$(this).parent().addClass('active');
     	}
     }); 
 };
 
-
+*/
 /** update sidebar */
-var lion = $('#lion').offset().top;
-var calFlame = $('#cal-flame').offset().top;
-var fireMagic = $('#fire-magic').offset().top;
+var lion = $('#lion').offset().top - 5;
+var calFlame = $('#cal-flame').offset().top - 5;
+var fireMagic = $('#fire-magic').offset().top - 5;
 
 
 $(window).scroll(function(){
-
 	//get container scroll position
 	var fromTop = $(this).scrollTop();
 
